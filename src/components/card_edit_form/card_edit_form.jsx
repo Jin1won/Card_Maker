2432c4ce3bbf2a95a2d6464ea3bc23af2a4b9167
Jin1won <wlsdnjs16521@gmail.com>
2021-07-02 16:02:49 +0900

    add card and edit form component

---
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './card_edit_form.module.css';
+import Button from '../button/button';
+import ImageFileInput from '../image_file_input/image_file_input';
+
+const CardEditForm = ({card}) => {
+    const {name, company, theme, title, email, message, fileName, fileURL} = card;
+    
+    const onSubmit = () => {
+
+    }
+    
+    return(
+        <form className={styles.form}> 
+            <input className={styles.input} name="name" type="text" value={name}/>
+            <input className={styles.input} name="company" type="text" value={company}/>
+            <select className={styles.select} name="theme">
+                <option value="dark">dark</option>
+                <option value="light">light</option>
+                <option value="colorful">colorful</option>
+            </select>
+            <input className={styles.input} name="title" type="text" value={title}/>
+            <input className={styles.input} name="email" type="text" value={email}/>
+            <textarea className={styles.textarea} name="message" type="text" value={message}/>
+        <div className={styles.fileInput}>
+            <ImageFileInput fileName={fileName}/>
+        </div>
+        <Button name="Delete" onClick={onSubmit}/>
+        </form>
+    );
+};
+
+export default CardEditForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,12 +1,50 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useHistory } from 'react-router-dom';
 import styles from './maker.module.css';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import Preview from '../preview/preview';
+import Editor from '../editor/editor';
 
 const Maker = ({authService}) => {
-    const history = useHistory();
+//class로 react 쓸 때 state에 object 넣었던 것 처럼 useState안에도 넣을 수 있다.
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'Jin1',
+            company: 'Seoultech',
+            theme: 'dark',
+            title: 'Front-end Developer',
+            email: 'wlsdnjs16521@gmail.com',
+            message: 'go for it',
+            fileName: 'jinwon',
+            fileURL: null,
+        },
+        {
+            id: '2',
+            name: 'Jin2',
+            company: 'Seoultech',
+            theme: 'light',
+            title: 'Front-end Developer',
+            email: 'wlsdnjs16521@gmail.com',
+            message: 'go for it',
+            fileName: 'jinwon',
+            fileURL: null,
+        },
+        {
+            id: '3',
+            name: 'Jin3',
+            company: 'Seoultech',
+            theme: 'colorful',
+            title: 'Front-end Developer',
+            email: 'wlsdnjs16521@gmail.com',
+            message: 'go for it',
+            fileName: 'jinwon',
+            fileURL: null,
+        }
+    ]);
 
+    const history = useHistory();
     const onLogout = () => {
         authService.logout();
     }
@@ -22,7 +60,10 @@ const Maker = ({authService}) => {
     return(
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
-            <h1>Maker</h1>
+            <div className={styles.container}>
+                <Editor cards={cards}/>
+                <Preview cards={cards}/>
+            </div>
             <Footer />
         </section>
     );
